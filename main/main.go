401fb411e274ccedd9c58fbf2aab3db7e6bf5e76
richardsahvic <richsegar1208@yahoo.com>
2018-08-13 10:51:47 +0700

    repo-pattern

--- main/main.go
+++ main/main.go
@@ -17,29 +17,24 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-type LoginRequest struct {
-	Username string `json: "username"`
-	Password string `json: "password"`
-	Role     int    `json: "role"`
+type loginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Role     int    `json:"role"`
 }
 
-type Response struct {
-	Message string `json: "message"`
+type response struct {
+	Message string `json:"message"`
 }
 
-type RegisterRequest struct {
-	ID       string `json: "id"`
-	Email    string `json: "email"`
-	Msisdn   string `json: "msisdn"`
-	Username string `json: "username"`
-	Password string `json: "password"`
-	Status   int    `json: "status"`
-	Role     int    `json: "role"`
-}
-
-type ProfileRequest struct {
-	Token string `json: "token"`
-	Email string `json: "email"`
+type registerRequest struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Msisdn   string `json:"msisdn"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Status   int    `json:"status"`
+	Role     int    `json:"role"`
 }
 
 var userService service.UserService
@@ -70,23 +65,23 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
 	body, _ := ioutil.ReadAll(io.LimitReader(r.Body, 5000))
 
-	var loginRequest LoginRequest
-	json.Unmarshal(body, &loginRequest)
+	var loginReq loginRequest
+	json.Unmarshal(body, &loginReq)
 
-	loginResult, err := userService.Login(loginRequest.Username, loginRequest.Password, loginRequest.Role)
+	loginResult, err := userService.Login(loginReq.Username, loginReq.Password, loginReq.Role)
 	if err != nil {
 		log.Println("Failed at login,   ", err)
 	}
 
-	var loginResponse Response
+	var loginResp response
 
 	if len(loginResult) == 0 {
-		loginResponse.Message = "Login failed"
+		loginResp.Message = "Login failed"
 	} else {
-		loginResponse.Message = "Login Success"
+		loginResp.Message = "Login Success"
 	}
 
-	js, err := json.Marshal(loginResponse)
+	js, err := json.Marshal(loginResp)
 	if err != nil {
 		log.Println("ERROR at login marshal,    ", err)
 	}
@@ -101,8 +96,8 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 
 	body, _ := ioutil.ReadAll(io.LimitReader(r.Body, 5000))
 
-	var registerRequest RegisterRequest
-	json.Unmarshal(body, &registerRequest)
+	var regRequest registerRequest
+	json.Unmarshal(body, &regRequest)
 
 	node, err := snowflake.NewNode(1)
 	if err != nil {
@@ -114,29 +109,29 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 
 	userRegister := repo.User{
 		ID:       id,
-		Email:    registerRequest.Email,
-		Msisdn:   registerRequest.Msisdn,
-		Username: registerRequest.Username,
-		Password: registerRequest.Password,
+		Email:    regRequest.Email,
+		Msisdn:   regRequest.Msisdn,
+		Username: regRequest.Username,
+		Password: regRequest.Password,
 		Status:   0,
 	}
 
-	role := registerRequest.Role
+	role := regRequest.Role
 
 	registerResult, err := userService.Register(userRegister, role)
 	if err != nil {
 		log.Println("failed to register,    ", err)
 	}
 
-	var registerResponse Response
+	var regResponse response
 
 	if !registerResult {
-		registerResponse.Message = "Register failed"
+		regResponse.Message = "Register failed"
 	} else {
-		registerResponse.Message = "Register success"
+		regResponse.Message = "Register success"
 	}
 
-	json.NewEncoder(w).Encode(registerResponse)
+	json.NewEncoder(w).Encode(regResponse)
 }
 
 func profileHandler(w http.ResponseWriter, r *http.Request) {
